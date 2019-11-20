ff5fa9d30dcec38ec07f8752135d8f6b380d33b1
moussa <moussa9594@gmail.com>
2019-11-20 14:43:13 +0000

    modification 3 de la classe de teste

--- src/mesClassesTeste/TesteCalcul.java
+++ src/mesClassesTeste/TesteCalcul.java
@@ -1,7 +1,6 @@
 package mesClassesTeste;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -9,25 +8,26 @@ import mesClasses.Calcul;
 
 public class TesteCalcul {
 
-  //creation de l'objet	
-	Calcul c1 = new Calcul();
-	                                                                                          
-	@Test                                                          
-	public void testAddition() {                                   
-	//teste de la methode addition                                 
-		assertEquals(4, c1.addition(2, 2), 0);                     
-		assertEquals(3.5, c1.addition(2, 1.5), 0);                 
-		assertEquals(-3, c1.addition(-6, 3), 0);                   
-		assertEquals(0, c1.addition(2.4, -2.4), 0);                
-	}                                                              
-                                                                   
-	@Test                                                          
-	public void testMultiplication() {                             
-		//teste de la methode multiplication                       
-				assertEquals(4, c1.multiplication(2, 2), 0);             
-				assertEquals(3, c1.multiplication(2, 1.5), 0);         
-				assertEquals(-18, c1.multiplication(-6, 3), 0);           
-				assertEquals(-5.76, c1.multiplication(2.4, -2.4), 0);
-	}
+
+	  //creation de l'objet	
+		Calcul c1 = new Calcul();
+		                                                                                          
+		@Test                                                          
+		public void testAddition() {                                   
+		//teste de la methode addition                                 
+			assertEquals(4, c1.addition(2, 2), 0);                     
+			assertEquals(3.5, c1.addition(2, 1.5), 0);                 
+			assertEquals(-3, c1.addition(-6, 3), 0);                   
+			assertEquals(0, c1.addition(2.4, -2.4), 0);                
+		}                                                              
+	                                                                   
+		@Test                                                          
+		public void testMultiplication() {                             
+			//teste de la methode multiplication                       
+					assertEquals(4, c1.multiplication(2, 2), 0);             
+					assertEquals(3, c1.multiplication(2, 1.5), 0);         
+					assertEquals(-18, c1.multiplication(-6, 3), 0);           
+					assertEquals(-5.76, c1.multiplication(2.4, -2.4), 0);
+		}
 
 }
