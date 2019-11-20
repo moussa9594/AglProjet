2611c8ca20e2fc4415216f58b9b30a6192795ee0
moussa <moussa9594@gmail.com>
2019-11-20 14:20:10 +0000

    modification de la classe de teste

--- src/mesClassesTeste/TesteCalcul.java
+++ src/mesClassesTeste/TesteCalcul.java
@@ -1,10 +1,9 @@
 package mesClassesTeste;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import mesClasses.Calcul;
